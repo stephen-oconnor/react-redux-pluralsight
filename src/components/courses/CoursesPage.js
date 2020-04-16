44efef7482615994d5f6665ceee92ea1dfa63b40
Stephen O'Connor <stephenoconnor01@googlemail.com>
2020-04-16 13:59:42 +0100

    Setup.

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,75 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import CourseList from "./CourseList";
+
+class CoursesPage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, actions } = this.props;
+
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert("Loading courses failed: " + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert("Loading authors failed: " + error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>
+        <CourseList courses={this.props.courses} />
+      </>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+// Expose redux store to component props. Called when store updates.
+function mapStateToProps(state) {
+  return {
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
+  };
+}
+
+// Expose actions to component props. Must be wrapped in dispatch. Good option.
+function mapDispatchToProps(dispatch) {
+  return {
+    // bindActionCreators will wrap each action in dispatch.
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
+  };
+}
+
+// When mapDispatchToProps is an Object each property is expected to be an action creator function. connect will bind properties automatically.
+// const mapDispatchToProps = {
+//   createCourse: courseActions.createCourse,
+// };
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
---
+++ src/redux/actions/authorActions.js
@@ -0,0 +1,23 @@
+import * as types from "./actionTypes";
+import * as authorApi from "../../api/authorApi";
+
+export function loadAuthorsSuccess(authors) {
+  return {
+    type: types.LOAD_AUTHORS_SUCCESS,
+    authors: authors,
+  };
+}
+
+// Thunk(s).
+export function loadAuthors() {
+  return function (dispatch) {
+    return authorApi
+      .getAuthors()
+      .then((authors) => {
+        dispatch(loadAuthorsSuccess(authors));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
